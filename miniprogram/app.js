4800db69430b0d2bcea1b798217c7fec4918950b
zhouyuefei <zhouyuefei20@163.com>
2020-08-06 10:41:10 +0800

    123

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -8,6 +8,7 @@ App({
     } else {
 		console.log('你好mini_a');
     console.log('是谁');
+      console.log('是谁1');
       wx.cloud.init({
         // env 参数说明：
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
